fd1825124b703248a842c356aabb1bbb149b9028
Amar Shishodia <amar.shishodia@gmail.com>
2017-06-06 12:29:10 +0530

    Initial commit-2

---
+++ polls/admin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import Choice,Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_filter = ['pub_date']
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+

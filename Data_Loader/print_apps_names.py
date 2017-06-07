5ffb1b9648c6f32069e7b52b50e74cbdac45d9af
Amar Shishodia <amar.shishodia@gmail.com>
2017-06-07 09:31:00 +0530

    Update print_apps_names.py

--- Data_Loader/print_apps_names.py
+++ Data_Loader/print_apps_names.py
@@ -1,7 +1,6 @@
 import settings
-#print(settings.INSTALLED_APPS)
 data = settings.INSTALLED_APPS
 import json
 
-with open('apps.json','w') as outfile:
+with open('app_name_class_name.json','w') as outfile:
     json.dump(data,outfile)
